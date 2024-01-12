2b1aca9061d2f7751116bf49fd8b65c1a220ffc8
Genju <derfloh205@gmail.com>
2024-01-12 22:43:42 +0100

    v up

--- GGUI.lua
+++ GGUI.lua
@@ -1,7 +1,7 @@
 
 
 ---@class GGUI-2.0
-local GGUI = LibStub:NewLibrary("GGUI-2.0", 14)
+local GGUI = LibStub:NewLibrary("GGUI-2.0", 15)
 if not GGUI then return end -- if version already exists
 
 local GUTIL = GGUI_GUTIL
